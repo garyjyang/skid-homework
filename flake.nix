526c3d1d9e3eca683f9964b57cc3039fd61a7db0
cubewhy <qby140326@gmail.com>
2026-02-13 11:33:36 +0800

    chore: optimize flake.nix

--- flake.nix
+++ flake.nix
@@ -5,28 +5,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     blackbox.url = "github:cubewhy/blackbox-flakes";
-
-    #: Do not forget to modify the `overlays` variable below after you added new overlays
-
-    #: Uncomment if you need Rust
-    # rust-overlay.url = "github:oxalica/rust-overlay";
-
-    #: Uncomment if you need Golang
-    # go-overlay.url = "github:purpleclay/nix-go";
   };
 
   outputs = {
     self,
     blackbox,
-    # rust-overlay,
-    # go-overlay,
     nixpkgs,
     ...
   }: let
-    overlays = [
-      # (import rust-overlay)
-      # (import go-overlay)
-    ];
+    overlays = [];
   in {
     devShells =
       blackbox.lib.eachSystem {
@@ -37,33 +24,7 @@
 
           #: Config {{{
           config = {
-            #: Note: change the options there
-            #: You can delete unused options
-
             #: Languages {{{
-            #: Rust {{{
-            blackbox.languages.rust = {
-              enable = false;
-              #: version: available values ["stable" "beta" "nightly" "nightly-<date>"]
-              version = "stable";
-              components = ["rustc" "cargo" "clippy" "rustfmt" "rust-analyzer"];
-              #: any rust targets, like x86_64-pc-windows-gnu, leave blank to use platform default
-              #: the blackbox flake contains the Windows cross-compile workaround (pthreads).
-              #: But please notice that you may still need to tackle with 3rd party libraries like
-              #: openssl
-              targets = [
-                # "x86_64-pc-windows-gnu"
-              ];
-            };
-            #: }}}
-
-            #: C/C++ {{{
-            blackbox.languages.c = {
-              enable = false;
-              #: compiler: available values ["gcc" "clang"]
-              compiler = "gcc";
-            };
-            #: }}}
 
             #: Javascript/Typescript {{{
             #: tags: javascript, typescript, js, ts, nodejs, npm, pnpm, yarn
@@ -75,54 +36,6 @@
               manager = "pnpm";
               #: Auto run `npm install` (or with other package managers) if package.json exist
               autoInstall = true;
-            };
-            #: }}}
-
-            #: Java {{{
-            blackbox.languages.java = {
-              enable = false;
-              package = pkgs.jdk;
-
-              #: Java build tools
-              maven = {
-                enable = false;
-                package = pkgs.maven;
-              };
-
-              gradle = {
-                enable = false;
-                package = pkgs.gradle;
-              };
-            };
-            #: }}}
-
-            #: Golang {{{
-            blackbox.languages.go = {
-              enable = false;
-              version = "latest";
-              #: enabled installTools option to install gopls, delve, golangci-lint, gotools
-              installTools = false;
-            };
-            #: }}}
-
-            #: }}}
-
-            #: Libraries {{{
-            blackbox.libraries = {
-              #: OpenSSL {{{
-              openssl.enable = false;
-              #: }}}
-
-              #: Cuda: {{{
-              #: See https://nixos-cuda.org/ for enabling nix cuda cache
-              cuda = {
-                enable = false;
-                #: version: e.g.  [11, 12, 13]
-                version = "13";
-                #: Enable this to install nvidia_x11 package
-                withDrivers = true;
-              };
-              #: }}}
             };
             #: }}}
 
